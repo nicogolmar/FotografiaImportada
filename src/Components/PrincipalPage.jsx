ace20cb55224facfc7b979a37a7ce6c47673bdc4
Nicolas Golmar <nico.bocakapo09@gmail.com>
2024-08-26 23:44:13 -0300

    cambio responsive a 2 columnas

--- src/Components/PrincipalPage.jsx
+++ src/Components/PrincipalPage.jsx
@@ -142,7 +142,7 @@ function PrincipalPage({pantallaEnviada}) {
           </Dropdown>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {photos.map((photo) => (
            
             <Card 
